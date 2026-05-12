e011ffaf6c9f927b6e3e119e205d74f79a8c9d6d
Haran <haran@nxdomain.ca>
2026-05-12 16:38:02 -0400

    first pass

---
+++ scratchpad/main.js
@@ -0,0 +1,43 @@
+// aria-* = screen reader (high-level, oversimplified)
+const search = document.querySelector("textarea,input[aria-label=Search]")
+// textarea,input[name=q] is also an option, but it also looks more likely to change/be duplicated
+// search.value contains the search query (after rendering js? who knows, but the HTML already has it)
+
+// thankfully, we don't actually need this - this time.
+const infobar = document.querySelector("div.kp-wholepage-osrp");
+// infobar contains the "Vincent Van Gogh (Dutch painter)"
+
+const kgStuff = {
+  
+}
+
+const results = document.querySelector("div#search"); // not a fan, but this seems stable
+
+// const botstuff = document.querySelector("div#botstuff") // anti-bot stuff?
+
+// const artblock = document.querySelector("div[data-hveid=2ahUKEwjK-K-JwLWKAxXcQTABHePpOFoQy9oBKAB6BAg8EAA]"); // yeah nah not this
+
+// const artblockOnlyDirect = document.querySelector("div[attrid=kc:/visual_art/visual_artist:works]") // not this either
+
+const artworkResults = results.querySelector("g-loading-icon").nextElementSibling; // Interesting.
+// works on live too, hmm
+// but NOT on discography
+//  NOTE: discography uses a different block - the GRID view, not a carousel view - (wp-grid-view, wp-grid-tile)
+//    this grid view, given the more predictable element name, has different approaches
+// same thing for filmography, games, books
+
+// can easily be deprecated if Google decides to recompile with more mangling and nesting
+// const artworkParent = artworkResults.parentElement.parentElement;
+
+const paintings = Array.from(artworkResults.querySelectorAll("img"))
+  .map(img => {
+    const details = img.nextElementSibling; // relative HTML positioning, can be deprecated easily
+    return {
+      name: details.firstElementChild?.textContent,
+      extensions: [
+        details.lastElementChild?.textContent
+      ],
+      link: img.parentElement.href,
+      image: img.src,
+    }
+  })
---
+++ scratchpad/notes.md
@@ -0,0 +1,9 @@
+# assumed pipeline
+
+user -> serpapi (black-box)
+
+serapi -> browser/fetcher -> proxy/catpchas -> html
+
+html -> scrape -> scrape result
+
+scrape result -> user
