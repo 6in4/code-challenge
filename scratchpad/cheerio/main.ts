b200fc0a684540b5ba6b33112a6e515faeec3a68
Haran <haran@nxdomain.ca>
2026-05-15 18:56:44 -0400

    Cleanup prototype, add more test pages

--- scratchpad/cheerio/main.ts
+++ scratchpad/cheerio/main.ts
@@ -7,10 +7,10 @@ type Results = {
         extensions: string[] | undefined;
         link: string;
         image: string;
-    }
+    }[];
 };
 
-function scrapeCarousel($: cheerio.CheerioAPI) {
+export function scrapeCarousel($: cheerio.CheerioAPI): Results {
     // result root - not really necessary though
     const results = $("div#search");
     // anchor element is the carousel container 
@@ -18,12 +18,13 @@ function scrapeCarousel($: cheerio.CheerioAPI) {
     const carousel = $("[data-attrid=\"kc:/visual_art/visual_artist:works\"]", results);
 
     const items = $("img", carousel).map((_, img) => {
-        const details = img.nextSibling;
-        if (!details) {
-            // log/raise for o11y
-            throw new Error();
+        const details = $("div", $(img).parent())
+        if (details.length === 0) {
+            // work details missing! possible structure change?
+            throw new Error("image details missing - structure changed?");
         }
 
+        // name is first, year (if present) is last
         const name = $(details).children().first().text().trim();
         const maybeYearText = $(details).children().last().text();
         const hasExtensions = maybeYearText.trim().length !== 0;
@@ -31,9 +32,11 @@ function scrapeCarousel($: cheerio.CheerioAPI) {
             ? [ maybeYearText.trim() ] 
             : undefined;
 
+        // when data-src is present, it will contain a URL to src
+        // - src will be a blank gif
         let image = $(img).attr("data-src") ?? $(img).attr("src");
         // images injected by a script
-        // TODO: this will break gifs
+        // this will not work if the image is truly a gif
         if (image?.startsWith("data:image/gif;")) {
             const imageId = $(img).attr("id");
             if (!imageId) {
@@ -41,37 +44,38 @@ function scrapeCarousel($: cheerio.CheerioAPI) {
                 throw new Error();
             }
 
-            // ~~narrow down to specific script, using non-greedy quantifiers~~
-            // just grab the two variables -_-
-            // this WILL break if they change the script order
-            // console.log(`var s='data:image/[a-z]+;base64,([\\\\A-Za-z0-9/+=]+)';var ii=\\['${imageId}'\\];`)
-            // yes, the comma IS in the rfc
-            const scriptData = $.html().match(new RegExp(`var s='(data:[a-z]+/[a-z]+;base64,[^']+)';var ii=\\['${imageId}'\\];`));
+            // this WILL break if they change the variable order in the script
             // assume that `var ii=['<id>']` is stable enough
+            // also, yes, this is expensive relative to building a map in advance
+            const scriptData = $.html().match(new RegExp(`var s='(data:[a-z]+/[a-z]+;base64,[^']+)';var ii=\\['${imageId}'\\];`));
             if (scriptData && scriptData.length == 2) {
                 image = scriptData[1]!;
-                console.log(image.substring(image.length - 5, ));
                 image = image.replace(/\\x3d/g, "=");
             } else {
-                console.log(imageId);
+                console.error(imageId, );
+                // increase the error rate - 
+                // something needs to be fixed ASAP
+                throw new Error("img.id is missing");
             }
-            // const imageData = scriptData.
         }
 
+        // at this point, this should either be a jpeg or a cdn url - not a gif
+        if (image?.startsWith("data:image/gif")) {
+            throw new Error(`incorrect image type (name=${name})`);
+        }
 
         const data = {
             name,
-            extensions: extensions === undefined ? undefined : extensions, // what?
+            extensions,
             // parentNode is carousel, by definition
             // also, the <a> is assumed to be relative
             link: (
+                // to match expected-array.json (vs the file:// url)
                 "https://www.google.com" 
                 + $(img.parentNode!).attr("href")
             ),
-            image,
+            image: image!, // could also be string | undefined
         }
-
-        // console.log(name, `"${maybeYearText}"`, maybeYearText.trim().length, extensions, data.extensions);
 
         return data;
     });
@@ -82,13 +86,14 @@ function scrapeCarousel($: cheerio.CheerioAPI) {
 
 async function main() {
     if (process.argv.length !== 3) {
-        console.error(`USAGE: ${process.argv0} <file.html>`);
+        console.log(`USAGE: ${process.argv0} <file.html>`);
+        console.log(`\tOR: npm start -- <file.html>`);
+        process.exit(1);
     }
 
     const html = await readFile(process.argv[2]!, { encoding: "utf-8" });
     const $ = cheerio.load(html);
-    scrapeCarousel($).artworks.forEach(e => console.log(e.name, e.extensions))
-    console.error(JSON.stringify(scrapeCarousel($), null, 2));
+    console.log(JSON.stringify(scrapeCarousel($), null, 2));
 }
 
 
